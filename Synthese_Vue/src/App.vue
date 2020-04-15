b95a6ffb87f009aaa23a64f48fc9fe839ca56085
CocoCouq <couq@icloud.com>
2020-04-15 20:06:12 +0200

    Start small Vue Project

---
+++ Synthese_Vue/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <index/>
+    <addition v-if="appAdd"/>
+    <magic v-if="appMag"></magic>
+    <calculator v-if="appCal"></calculator>
+    <changebox v-if="appInt"></changebox>
+  </div>
+</template>
+
+<script>
+  import index from './components/index'
+  import addition from './components/addition'
+  import magic from './components/magic'
+  import calculator from './components/calculator'
+  import changebox from './components/interface'
+
+export default {
+  name: 'App',
+  components: {
+    index,
+    addition,
+    magic,
+    calculator,
+    changebox
+  },
+  data () {
+    return {
+      appAdd: false,
+      appMag: false,
+      appCal: false,
+      appInt: true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Synthese_Vue/src/components/navbar.vue
@@ -0,0 +1,51 @@
+<template>
+      <nav>
+        <div class="nav-wrapper">
+        <ul class="row">
+          <li class="green lighten-2 col s3" v-for="btnN in btnsNavbar">
+            <i class="material-icons" v-on:click="btnN.actionBtn">{{btnN.icon}}</i>
+          </li>
+        </ul>
+        </div>
+      </nav>
+</template>
+
+<script>
+  export default {
+    name: 'navbar',
+    data () {
+      return {
+        btnsNavbar: [
+          {icon: 'add', actionBtn: this.addFunction},
+          {icon: 'golf_course', actionBtn: this.magFunction},
+          {icon: 'keyboard', actionBtn: this.calFunction},
+          {icon: 'feedback', actionBtn: 'En attente'}
+        ]
+      }
+    },
+    methods: {
+      addFunction: function () {
+        // Toggle boolean
+        this.$parent.$parent.appAdd ^= true;
+        this.$parent.$parent.appMag = false;
+        this.$parent.$parent.appCal = false;
+      },
+      magFunction: function() {
+        this.$parent.$parent.appMag ^= true;
+        this.$parent.$parent.appAdd = false;
+        this.$parent.$parent.appCal = false;
+      },
+      calFunction: function() {
+        this.$parent.$parent.appCal ^= true;
+        this.$parent.$parent.appAdd = false;
+        this.$parent.$parent.appMag = false;
+      }
+    }
+  }
+</script>
+
+<style>
+  nav{
+    cursor: pointer;
+  }
+</style>
